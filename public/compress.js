211b2f6d95efe1c9f5edba73789dc0904ca70b88
miaowwhappy <183306832@qq.com>
2018-08-23 15:35:54 +0800

    tj

---
+++ public/compress.js
@@ -0,0 +1,52 @@
+/*
+    三个参数
+    file：一个是文件(类型是图片格式)，
+    obj：{
+        width: '',默认图片原始宽
+        height: '',默认图片原始高
+        quality: 0.7 默认,canvas转的图片质量
+    }
+    callback：回调函数(第一个参数是base64)
+*/
+
+function photoCompress(file,obj,callback){
+    var ready=new FileReader();
+    ready.readAsDataURL(file);
+    ready.onload=function(){
+        var re=this.result;
+        canvasDataURL(re,w,callback)
+    }
+}
+function canvasDataURL(path, obj, callback){
+    var img = new Image();
+    img.src = path;
+    img.onload = function(){
+        var that = this;
+        // 默认按比例压缩
+        var w = that.width,
+            h = that.height,
+            scale = w / h;
+        w = obj.width || w;
+        h = obj.height || (w / scale);
+        var quality = 0.7;  // 默认图片质量为0.7
+        //生成canvas
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        // 创建属性节点
+        var anw = document.createAttribute("width");
+        anw.nodeValue = w;
+        var anh = document.createAttribute("height");
+        anh.nodeValue = h;
+        canvas.setAttributeNode(anw);
+        canvas.setAttributeNode(anh);
+        ctx.drawImage(that, 0, 0, w, h);
+        // 图像质量
+        if(obj.quality && obj.quality <= 1 && obj.quality > 0){
+            quality = obj.quality;
+        }
+        // quality值越小，所绘制出的图像越模糊
+        var base64 = canvas.toDataURL('image/jpeg', quality);
+        // 回调函数返回base64的值
+        callback(base64);
+    }
+}
